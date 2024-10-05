490e0d013961d67fe76cb6ef079a607a7fea7c9c
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-05 16:33:14 +0300

    a lot of update

--- context/ThemeProvider.tsx
+++ context/ThemeProvider.tsx
@@ -10,17 +10,19 @@ interface ThemeContextType {
 const ThemeContext = createContext<ThemeContextType | undefined>(undefined);
 
 export function ThemeProvider({ children }: { children: React.ReactNode }) {
-  const [mode, setMode] = useState("light"); // Başlangıç durumunu "light" olarak ayarladık.
+  const [mode, setMode] = useState("");
 
   const handleThemeChange = () => {
-    if (mode === "dark") {
+    if (
+      localStorage.theme === "dark" ||
+      (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+    ) {
+      setMode("dark");
+      document.documentElement.classList.add("dark");
+    } else {
       setMode("light");
       document.documentElement.classList.remove("dark");
-      document.documentElement.classList.add("light");
-    } else {
-      setMode("dark");
-      document.documentElement.classList.remove("light");
-      document.documentElement.classList.add("dark");
     }
   };
 
@@ -41,5 +43,6 @@ export function useTheme() {
   if (context === undefined) {
     throw new Error("useTheme must be used within a ThemeProvider");
   }
+
   return context;
 }
