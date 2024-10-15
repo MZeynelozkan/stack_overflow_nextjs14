84529bbbad957568cc6c4f5676df693ae94bc8af
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-15 22:56:05 +0300

    ....

--- app/(root)/(home)/ask-question/page.tsx
+++ app/(root)/(home)/ask-question/page.tsx
@@ -11,7 +11,6 @@ const Page = async () => {
   if (!userId) redirect("/sign-in");
 
   const mongoUser = await getUserById({ userId });
-  console.log("hello world");
 
   if (!mongoUser) {
     console.log("Kullanıcı bulunamadı");
