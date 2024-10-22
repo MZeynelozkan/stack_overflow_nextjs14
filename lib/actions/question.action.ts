d246b797d0e6f157e67a14d9cb5c20c684d62677
MZeynelozkan <zeyzey4340@gmail.com>
2024-10-22 12:19:56 +0300

    a lot of improvement

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -3,7 +3,11 @@
 import Question from "@/database/question.model";
 
 import { connectToDatabase } from "../mongoose";
-import { CreateQuestionParams, GetQuestionsParams } from "./shared.types";
+import {
+  CreateQuestionParams,
+  GetQuestionByIdParams,
+  GetQuestionsParams,
+} from "./shared.types";
 import User from "@/database/user.model";
 import { revalidatePath } from "next/cache";
 import Tag from "@/database/tags.model";
@@ -60,4 +64,26 @@ export async function createQuestion(params: CreateQuestionParams) {
 
     revalidatePath(path);
   } catch (error) {}
+}
+
+export async function getQuestionById(params: GetQuestionByIdParams) {
+  try {
+    connectToDatabase();
+    const { questionId } = params;
+
+    const question = await Question.findById(questionId)
+      .populate({ path: "tags", model: Tag, select: "_id name" })
+      .populate({
+        path: "author",
+        model: User,
+        select: "_id clerkId name picture",
+      });
+
+    if (!question) throw new Error("Question not found");
+
+    return question;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
